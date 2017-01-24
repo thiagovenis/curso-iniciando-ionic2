0b7516db4d4beea37c91b83a6fa64811d0202a5e
Thiago Venis <thiago.venis@gmail.com>
2017-01-23 22:37:23 -0200

    capitulo 3 [v4]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,39 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage : any = HomePage;
+  pages: Array<{component: any, title: string, icon: string}>;
+
 
   constructor(platform: Platform) {
+
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'},
+      {component: MenuTestPage, title: 'Menu', icon: 'menu'}
+    ];
   }
+
+  clicou() : void {
+    console.log(this.pages);
+  }
+
+  openPage(page : any) : void {
+    console.log(page.component);
+    this.rootPage = page.component;
+  }
+
 }
---
+++ src/pages/menu-test/menu-test.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-menu-test',
+  templateUrl: 'menu-test.html'
+})
+export class MenuTestPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuTestPage');
+  }
+
+}
